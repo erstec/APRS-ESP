74b05c001d35895cf9c6a604ed53a4ced71461a2
Ernest (ErNis) <ernis@fpvdev.com>
2023-07-13 11:05:59 +0300

    wip

--- include/pinout.h
+++ include/pinout.h
@@ -70,6 +70,61 @@
 
 #endif /* BOARD_ESP32DR */
 
+#if defined(BOARD_ESP32DR_MOD_V14)
+
+// SA8x8 radio module
+#define POWER_PIN           25
+#define POWERDOWN_PIN       27
+#define SQL_PIN             33
+
+// Interface to the radio / radio module
+#define SPK_PIN             ADC1_CHANNEL_0  // Read ADC1_0 From PIN 36(VP)
+#define MIC_PIN             26              // Out wave to PIN 26
+#define PTT_PIN             32
+#define INVERT_PTT
+#define RX_LED_PIN          4//2 // IO4 - NC
+#define TX_LED_PIN          2//4
+#define INVERT_LEDS
+
+// ADC
+#define ADC_BATTERY         12  // Read ADC2_5 From PIN IO12, but only if wifi is turned off
+#define BATT_OFFSET         (-600) // mV
+
+// DEBUG UART
+#define SERIAL_DEBUG_BAUD   115200
+
+// RF UART
+#define SERIAL_RF_UART      1
+#define SERIAL_RF_BAUD      9600
+#define SERIAL_RF_RXPIN     14
+#define SERIAL_RF_TXPIN     13
+
+// GPS UART
+#define SERIAL_GPS_UART     2
+#define SERIAL_GPS_BAUD     9600
+#define SERIAL_GPS_RXPIN    15//16
+#define SERIAL_GPS_TXPIN    17
+
+// TNC UART
+#define SERIAL_TNC_UART     2
+#define SERIAL_TNC_BAUD     9600
+#define SERIAL_TNC_RXPIN    15//16
+#define SERIAL_TNC_TXPIN    17
+
+// I2C OLED
+#define OLED_WIDTH          128
+#define OLED_HEIGHT         64
+#define OLED_SDA_PIN        21
+#define OLED_SCL_PIN        22
+#define OLED_RST_PIN        -1
+
+// ROTARY ENCODER
+#define PIN_ROT_CLK         18
+#define PIN_ROT_DT          19
+#define PIN_ROT_BTN         5   // IO0 may be handled too?
+
+#endif /* BOARD_ESP32DR_MOD_V14 */
+
 #if defined(BOARD_TTWR)
 
 #define BOARD_NAME          "T-TWR"
--- src/main.cpp
+++ src/main.cpp
@@ -327,6 +327,7 @@ void setup()
     Serial.println("Start APRS-ESP V" + String(VERSION));
     Serial.println("Press and hold BOOT button for 3 sec to Factory Default config");
 
+#if defined(BOARD_ESP32DR_MOD_V14)
     // +4.2V EN
     pinMode(16, OUTPUT);
     digitalWrite(16, HIGH);
@@ -334,6 +335,7 @@ void setup()
     // OLED EN
     pinMode(23, OUTPUT);
     digitalWrite(23, HIGH);
+#endif
 
 #if defined(ADC_BATTERY)
     // Battery Voltage
